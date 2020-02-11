5958fbae11891889fd232c45397a0eef2a5f5e5f
iwanjeriatno <jeriat01@gmail.com>
2020-02-11 10:21:44 +0700

    +

--- database/migrations/2020_02_11_004751_update_users_table_api_token.php
+++ database/migrations/2020_02_11_004751_update_users_table_api_token.php
@@ -14,7 +14,7 @@ class UpdateUsersTableApiToken extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('api_token')->after('email');
+            $table->string('api_token')->after('email')->nullable();
         });
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- Styles -->
         <style>
@@ -70,12 +71,15 @@
 
             <div class="content">
                 <div class="bg">
-                    <img src="{{ asset('laravel-fractal.png') }}" alt="laravel-fractal">
+                    <img src="{{ asset('laravel-fractal.png') }}" alt="laravel-fractal" class="img-fluid">
                 </div>
                 <div class="link">
-                    <a href="https://medium.com/@jeriatno/laravel-fractal-cbe9745e14f4">Visit this article...</a>
+                    <a href="https://medium.com/@jeriatno/laravel-fractal-cbe9745e14f4" target="_blank">Visit this article...</a>
                 </div>
             </div>
         </div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
